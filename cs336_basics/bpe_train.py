1f364065e091fef2906e22a1cc6ef83b02671612
youngcr <974227915@qq.com>
2026-03-26 22:06:30 +0800

    feat: word count dedup

--- cs336_basics/bpe_train.py
+++ cs336_basics/bpe_train.py
@@ -1,7 +1,8 @@
-import os
+import os, json
 import logging
 import regex as re
 from collections.abc import Iterator
+from collections import Counter
 from multiprocessing import Pool
 from cs336_basics.pretokenization_example import find_chunk_boundaries
 
@@ -52,11 +53,11 @@ def train_bpe(
     log.debug(f"split into {len(chunks)} chunks")
     # 3. Parallel pre-tokenize
     with Pool(CPU_NUM) as p:
-        pre_token_array = p.map(pre_tokenize, chunks)
-    log.debug(f"pre-tokenize done, got {len(pre_token_array)} pre tokens")
+        pre_token_chunks = p.map(pre_tokenize, chunks)
+    log.debug(f"pre-tokenize done, got {len(pre_token_chunks)} pre token chunks")
     # 4. Generate initialize token pair count and index
-    pre_tokens = [t for pre_token in pre_token_array for t in pre_token]
-    pair_count, pair_index = initialize_token_pair_count_and_index(pre_tokens)
+    word_count = get_word_count(pre_token_chunks)
+    pair_count, pair_index = initialize_token_pair_count_and_index(word_count)
     log.debug(f"initialize token pair count and index done, got {len(pair_count)} token pairs")
     # 5. Iteratively count highest token pair, update pair_count and pair_index
     while len(vocabulary) < vocab_size:
@@ -68,12 +69,23 @@ def train_bpe(
         log.debug(f"{len(merges)} merge {merges[-1]}, vocab size is {len(vocabulary)}")
         # update pair_count and pair_index
         pair_count, pair_index = update_token_pair_count_and_index(highest_pair, pair_count, pair_index)
-        log.debug(f"{len(merges)} update token pair count and index done, got {len(pair_count)} token pairs")
+        log.debug(f"{len(merges)} update token pair count and index done, {len(pair_count)} token pairs")
     vocab = {}
     for i in range(len(vocabulary)):
         vocab[i] = vocabulary[i]
     return vocab, merges
 
+def get_word_count(word_chunks: list[list[str]]) -> Counter:
+    """
+    different token pairs in identical words have the same effect 
+    dedup by word count improve training efficiency
+    """
+    word_count = Counter()
+    for chunk in word_chunks:
+        for word in chunk:
+            word_count[word] += 1
+    return word_count
+    
 
 def initalize_vocabulary(special_tokens: list[str]) -> list[bytes]:
     """
@@ -111,53 +123,55 @@ def pre_tokenize(data: str, pattern: str = PAT) -> list[str]:
     return [m.group() for m in re.finditer(pattern, data)]
 
 
-def initialize_token_pair_count_and_index(pre_tokens: Iterator[str]):
+def initialize_token_pair_count_and_index(word_count: Counter) -> tuple[Counter, dict[tuple[bytes, bytes], Counter]]:
     """
     count and index all token pairs, return
         pairs_count: counter of all token pairs
         pairs_index: index of all token pairs (set)
     """
-    pairs_count = {}
+    pairs_count = Counter()
     pairs_index = {}
     start = 0
-    for token in pre_tokens:
-        b = token.encode("utf-8")
-        token_bytes = len(b)
-        for i in range(token_bytes - 1):
+    for word in word_count:
+        b = word.encode("utf-8")
+        word_bytes = len(b)
+        for i in range(word_bytes - 1):
             pair = (b[i : i + 1], b[i + 1 : i + 2])
-            if pairs_count.get(pair) == None:
-                pairs_count[pair] = 0
-                pairs_index[pair] = set()
-            pairs_count[pair] += 1
-            pairs_index[pair].add(start + i)
-        start += token_bytes
+            if pair not in pairs_index:
+                pairs_index[pair] = Counter()
+            pairs_count[pair] += word_count[word]
+            pairs_index[pair][start + i] += word_count[word]
+        start += word_bytes
+    with open('cs336_basics/pairs_index.json', 'w') as f:
+        json.dump({str(k): v for k, v in pairs_index.items()}, f, indent=4)
     return pairs_count, pairs_index
 
 
-def get_highest_token_pair(pair_count: dict[tuple[bytes, bytes], int]) -> tuple[bytes, bytes]:
+def get_highest_token_pair(pair_count: Counter) -> tuple[bytes, bytes]:
     """
     get token pair with highest frequency
     prefer the lexicographically greater pair when tie
     """
     highest_count = 0
     highest_pair = None
-    for pair, count in pair_count.items():
-        if count > highest_count:
+    for pair in pair_count:
+        if pair_count[pair] > highest_count:
             highest_pair = pair
-            highest_count = count
-        elif count == highest_count and pair > highest_pair:
+            highest_count = pair_count[pair]
+        elif pair_count[pair] == highest_count and pair > highest_pair:
             highest_pair = pair
     return highest_pair
 
+
 def update_token_pair_count_and_index(
     highest_pair: tuple[bytes, bytes],
-    pair_count: dict[tuple[bytes, bytes], int],
-    pair_index: dict[tuple[bytes, bytes], set],
+    pair_count: Counter,
+    pair_index: dict[tuple[bytes, bytes], Counter],
 ):
     """
     update token pair count and index after merge, return
-        updated_count
-        updated_index
+        updated_pair_count
+        updated_pair_index
     """
     updated_count = pair_count.copy()
     updated_index = pair_index.copy()
@@ -166,36 +180,31 @@ def update_token_pair_count_and_index(
             updated_count.pop(pair)
             updated_index.pop(pair)
         else:
-            # iterate highest pair's index
-            # iterate overlapped pair's index
-
             # two overlapping cases may be hit at the same time
             if pair[0] == highest_pair[1]:
                 merged_pair = (highest_pair[0] + highest_pair[1], pair[1])
                 highest_pair_first_token_bytes = len(highest_pair[0])
-                for idx in pair_index[pair].copy():    # iterate pair instead of highest_pair first, reducing iterate times
-                    if idx - highest_pair_first_token_bytes in pair_index[highest_pair]:
-                        if merged_pair not in updated_count:
-                            updated_count[merged_pair] = 0
-                            updated_index[merged_pair] = set()
-                        updated_count[merged_pair] += 1
-                        updated_index[merged_pair].add(idx - highest_pair_first_token_bytes)
-                        updated_count[pair] -= 1
-                        updated_index[pair].remove(idx)
+                # iterate pair instead of highest_pair first, reducing iterate times
+                for idx in pair_index[pair].copy():    
+                    count = pair_index[pair][idx]
+                    highest_pair_start = idx - highest_pair_first_token_bytes
+                    if highest_pair_start in pair_index[highest_pair]:
+                        if merged_pair not in updated_index:
+                            updated_index[merged_pair] = Counter()
+                        updated_count[merged_pair] += count
+                        updated_index[merged_pair][highest_pair_start] = count
+                        updated_count[pair] -= count
+                        updated_index[pair].pop(idx)
             if pair[1] == highest_pair[0]:
                 merged_pair = (pair[0], highest_pair[0] + highest_pair[1])
                 pair_first_token_bytes = len(pair[0])
                 for idx in pair_index[pair].copy():
+                    count = pair_index[pair][idx]
                     if idx + pair_first_token_bytes in pair_index[highest_pair]:
-                        if merged_pair not in updated_count:
-                            updated_count[merged_pair] = 0
-                            updated_index[merged_pair] = set()
-                        updated_count[merged_pair] += 1
-                        updated_index[merged_pair].add(idx)
-                        updated_count[pair] -= 1
-                        updated_index[pair].remove(idx)
-    for pair, count in list(updated_count.items()):
-        if count == 0:
-            updated_count.pop(pair)
-            updated_index.pop(pair)
+                        if merged_pair not in updated_index:
+                            updated_index[merged_pair] = Counter()
+                        updated_count[merged_pair] += count
+                        updated_index[merged_pair][idx] = count
+                        updated_count[pair] -= count
+                        updated_index[pair].pop(idx)
     return updated_count, updated_index
--- cs336_basics/test_bpe_train.py
+++ cs336_basics/test_bpe_train.py
@@ -9,7 +9,7 @@ SPECIAL_TOKEN = "<|endoftext|>"
     "input_path, chunks_num",
     [
         ("data/test_get_chunks_1.txt", 1),
-        ("data/test_get_chunks_2.txt", 2),
+        # ("data/test_get_chunks_2.txt", 2),
         ("data/TinyStoriesV2-GPT4-valid.txt", 12),
     ],
 )
@@ -33,7 +33,7 @@ def test_initalize_vocabulary(special_token, vocab_size):
     "input_path, expected_len",
     [
         ("data/test_get_chunks_1.txt", 209),
-        ("data/test_get_chunks_2.txt", 1131),
+        # ("data/test_get_chunks_2.txt", 1131),
     ],
 )
 def test_pre_tokenize(input_path, expected_len):
@@ -47,42 +47,41 @@ def test_pre_tokenize(input_path, expected_len):
     "input_path",
     [
         ("data/test_get_chunks_1.txt"),
-        ("data/test_get_chunks_2.txt"),
+        # ("data/test_get_chunks_2.txt"),
     ],
 )
 def test_initialize_token_pair_count_and_index(input_path):
     chunks = get_chunks([SPECIAL_TOKEN], 1, input_path)
     assert len(chunks) == 1
     pre_tokens = pre_tokenize(chunks[0])
-    pair_count, pair_index = initialize_token_pair_count_and_index(pre_tokens)
-    print(pair_count)
-    print(pair_index)
+    word_count = get_word_count([pre_tokens])
+    pair_count, pair_index = initialize_token_pair_count_and_index(word_count)
     assert len(pair_count) > 0
     assert len(pair_count) == len(pair_index)
     # validate pair_index includes all token pairs
     index_size = 0
-    for token in pre_tokens:
-        b = token.encode("utf-8")
+    for word in word_count:
+        b = word.encode("utf-8")
         index_size += len(b) - 1
     index_set = set()
-    for _, s in pair_index.items():
-        index_set = index_set.union(s)
+    for s in pair_index.values():
+        index_set = index_set.union(s.keys())
     assert index_size == len(index_set)
 
 
 @pytest.mark.parametrize(
     "input_path, pair_count, expected",
     [
-        ("data/test_get_chunks_1.txt", {}, (bytes([32]), bytes([116]))),
-        ("data/test_get_chunks_2.txt", {}, (bytes([32]), bytes([116]))),
+        ("data/test_get_chunks_1.txt", Counter(), (bytes([32]), bytes([116]))),
+        # ("data/test_get_chunks_2.txt", Counter(), (bytes([32]), bytes([116]))),
         (
             "",
-            {(bytes([32]), bytes([116])): 100, (bytes([100]), bytes([116])): 50},
+            Counter({(bytes([32]), bytes([116])): 100, (bytes([100]), bytes([116])): 50}),
             (bytes([32]), bytes([116])),
         ),
         (
             "",
-            {(bytes([32]), bytes([116])): 100, (bytes([100]), bytes([116])): 100},
+            Counter({(bytes([32]), bytes([116])): 100, (bytes([100]), bytes([116])): 100}),
             (bytes([100]), bytes([116])),
         ),
     ],
@@ -92,7 +91,8 @@ def test_get_highest_token_pair(input_path, pair_count, expected):
         chunks = get_chunks([SPECIAL_TOKEN], 1, input_path)
         assert len(chunks) == 1
         pre_tokens = pre_tokenize(chunks[0])
-        pair_count, _ = initialize_token_pair_count_and_index(pre_tokens)
+        word_count = get_word_count([pre_tokens])
+        pair_count, _ = initialize_token_pair_count_and_index(word_count)
     highest_pair = get_highest_token_pair(pair_count)
     assert highest_pair == expected
 
@@ -101,14 +101,15 @@ def test_get_highest_token_pair(input_path, pair_count, expected):
     "input_path, expected_highest_pair",
     [
         ("data/test_get_chunks_1.txt", (bytes([32]), bytes([116]))),
-        ("data/test_get_chunks_2.txt", (bytes([32]), bytes([116]))),
+        # ("data/test_get_chunks_2.txt", (bytes([32]), bytes([116]))),
     ],
 )
 def test_update_token_pair_count_and_index(input_path, expected_highest_pair):
     chunks = get_chunks([SPECIAL_TOKEN], 1, input_path)
     assert len(chunks) == 1
     pre_tokens = pre_tokenize(chunks[0])
-    pair_count, pair_index = initialize_token_pair_count_and_index(pre_tokens)
+    word_count = get_word_count([pre_tokens])
+    pair_count, pair_index = initialize_token_pair_count_and_index(word_count)
     highest_pair = get_highest_token_pair(pair_count)
     assert highest_pair == expected_highest_pair
     updated_count, updated_index = update_token_pair_count_and_index(highest_pair, pair_count, pair_index)
@@ -121,21 +122,29 @@ def test_update_token_pair_count_and_index(input_path, expected_highest_pair):
             # 32 116 100 44
             # 32 116
             # 116 100 44
-            {(bytes([32]), bytes([116])): 2, (bytes([116]), bytes([100])): 2, (bytes([100]), bytes([44])): 2},
-            {(bytes([32]), bytes([116])): {0, 4}, (bytes([116]), bytes([100])): {1, 6}, (bytes([100]), bytes([44])): {2, 7}},
+            Counter({(bytes([32]), bytes([116])): 2, (bytes([116]), bytes([100])): 2, (bytes([100]), bytes([44])): 2}),
+
+            {(bytes([32]), bytes([116])): Counter({0: 1, 4: 1}), (bytes([116]), bytes([100])): Counter({1: 1, 6: 1}), (bytes([100]), bytes([44])): Counter({2: 1, 7: 1})},
+
             (bytes([116]), bytes([100])),
-            {(bytes([32]), bytes([116])): 1, (bytes([32]), bytes([116, 100])): 1, (bytes([116, 100]), bytes([44])): 2},
-            {(bytes([32]), bytes([116])): {4}, (bytes([32]), bytes([116, 100])): {0}, (bytes([116, 100]), bytes([44])): {1, 6}},
+
+            Counter({(bytes([32]), bytes([116])): 1, (bytes([32]), bytes([116, 100])): 1, (bytes([116, 100]), bytes([44])): 2, (bytes([100]), bytes([44])): 0}),
+
+            {(bytes([32]), bytes([116])): Counter({0: 0, 4: 1}), (bytes([32]), bytes([116, 100])): Counter({0: 1}), (bytes([116, 100]), bytes([44])): Counter({1: 1, 6: 1}), (bytes([100]), bytes([44])): Counter()},
         ),
-                (
+        (
             # 32 116 100 44
             # 32 116
             # 116 100 44
-            {(bytes([32]), bytes([116])): 1, (bytes([32]), bytes([116, 100])): 1, (bytes([116, 100]), bytes([44])): 2},
-            {(bytes([32]), bytes([116])): {4}, (bytes([32]), bytes([116, 100])): {0}, (bytes([116, 100]), bytes([44])): {1, 6}},
+            Counter({(bytes([32]), bytes([116])): 1, (bytes([32]), bytes([116, 100])): 1, (bytes([116, 100]), bytes([44])): 2}),
+
+            {(bytes([32]), bytes([116])): Counter({4: 1}), (bytes([32]), bytes([116, 100])): Counter({0: 1}), (bytes([116, 100]), bytes([44])): Counter({1: 1, 6: 1})},
+
             (bytes([116, 100]), bytes([44])),
-            {(bytes([32]), bytes([116])): 1, (bytes([32]), bytes([116, 100, 44])): 1},
-            {(bytes([32]), bytes([116])): {4}, (bytes([32]), bytes([116, 100, 44])): {0}},
+
+            Counter({(bytes([32]), bytes([116])): 1, (bytes([32]), bytes([116, 100])): 0, (bytes([32]), bytes([116, 100, 44])): 1}),
+
+            {(bytes([32]), bytes([116])): Counter({4: 1}), (bytes([32]), bytes([116, 100])): Counter({0: 0}), (bytes([32]), bytes([116, 100, 44])): Counter({0: 1})},
         ),
     ],
 )
@@ -152,10 +161,10 @@ def test_update_token_pair_count_and_index_raw(
 @pytest.mark.parametrize(
     "input_path, vocab_size, special_tokens, expected_merge",
     [
-        # ("data/test_get_chunks_1.txt", 258, [SPECIAL_TOKEN], [(bytes([32]), bytes([116]))]),
+        ("data/test_get_chunks_1.txt", 258, [SPECIAL_TOKEN], [(bytes([32]), bytes([116]))]),
         # ("data/test_get_chunks_2.txt", 300, [SPECIAL_TOKEN], []),
-        # ("data/TinyStoriesV2-GPT4-valid.txt", 300, [SPECIAL_TOKEN], []),
-        ("data/TinyStoriesV2-GPT4-train-200M.txt", 300, [SPECIAL_TOKEN], []),
+        ("data/TinyStoriesV2-GPT4-valid.txt", 300, [SPECIAL_TOKEN], []),
+        # ("data/TinyStoriesV2-GPT4-train-200M.txt", 300, [SPECIAL_TOKEN], []),
     ],
 )
 def test_train_bpe(input_path, vocab_size, special_tokens, expected_merge):
